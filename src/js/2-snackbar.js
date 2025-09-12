6b3dd04971652375124f8c2f6d6f058461005815
VolodymyrV <kotsiubkov@gmail.com>
2025-09-12 14:31:26 +0300

    js-done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,46 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", handlerSubmit);
+
+
+
+
+function handlerSubmit(event) {
+    event.preventDefault();
+    const delay = Number(form.elements.delay.value);
+    const fielfildValue = form.elements.state.value;
+  
+
+    const promis = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (fielfildValue === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+
+    });
+    promis
+        .then((delay) => {
+            showToast(`✅ Fulfilled promise in ${delay}ms`, 'green');
+        })
+        .catch((delay) => {
+            showToast(`❌ Rejected promise in ${delay}ms`, 'red')
+        })
+    
+    function showToast(message, bgColor) {
+        iziToast.show({
+            message,
+            position: "topRight",
+            icon: "", // без іконки
+            backgroundColor: bgColor,
+            messageColor: "#ffffff",
+        });
+
+    };
+}
